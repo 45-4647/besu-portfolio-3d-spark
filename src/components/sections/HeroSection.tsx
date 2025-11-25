f1f10daa3456dfe05ee07934a4dcb6b85d2d9088
besufikad kasahun <besufikadkasahun12@gmail.com>
2025-11-25 04:13:07 -0800

    edit onr file

--- src/components/sections/HeroSection.tsx
+++ src/components/sections/HeroSection.tsx
@@ -84,7 +84,7 @@ export function HeroSection() {
           >
             <Download className="mr-2 h-5 w-5" />
 
-            <a className='text-blue-800' href="https://drive.google.com/file/d/1UZCUmcZmCrexTTpDFJzGm1ULghcxMjCO/view?usp=sharing" target="_blank"> Download CV</a>
+            <a className='text-blue-800' href="https://drive.google.com/file/d/12dt7hJzdDpPvRyQESstz9gYfLTNjrQU0/view?usp=sharing" target="_blank"> Download CV</a>
            
 
           </Button>
