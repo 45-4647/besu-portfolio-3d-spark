33b44143ec1218425d33f5372aa6d83ac9a1de8b
besufikad kasahun <besufikadkasahun12@gmail.com>
2026-01-29 06:44:22 -0800

    edit

--- src/components/sections/HeroSection.tsx
+++ src/components/sections/HeroSection.tsx
@@ -84,7 +84,7 @@ export function HeroSection() {
           >
             <Download className="mr-2 h-5 w-5" />
 
-            <a className='text-blue-800' href="https://drive.google.com/file/d/14ESkaN6W5bGpWFIuoYd9z0MmlAt22oow/view?usp=sharing" target="_blank"> Download CV</a>
+            <a className='text-blue-800' href="https://drive.google.com/file/d/1Sh0YtQ9iLUj_WjzTNKpy_v-T1k5z4nFJ/view?usp=sharing" target="_blank"> Download CV</a>
            
 
           </Button>
